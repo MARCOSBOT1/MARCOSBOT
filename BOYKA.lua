3e94d70d9c81ca09b687b25e0809cb7807066b98
MARCOSBOT1 <60098581+MARCOSBOT1@users.noreply.github.com>
2020-03-31 14:07:10 +0300

    Add files via upload

--- BOYKA.lua
+++ BOYKA.lua
@@ -7919,7 +7919,7 @@ end
 end
 
 -------------------------------
-if text == ""..(database:get(bot_id..'Name:Bot') or 'ماركوس').." غادر" or text == '' then  
+if text == ""..(database:get(bot_id..'Name:Bot') or 'ماركوس').." غادر" or text == 'مغادره' or text == 'مغادرة' then  
 if Sudo(msg) and not database:get(bot_id..'Left:Bot'..msg.chat_id_)  then 
 tdcli_function ({ID = "ChangeChatMemberStatus",chat_id_=msg.chat_id_,user_id_=bot_id,status_={ID = "ChatMemberStatusLeft"},},function(e,g) end, nil) 
 send(msg.chat_id_, msg.id_,'🔰| تم مغادرة المجموعه') 
